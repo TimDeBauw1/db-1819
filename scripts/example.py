31600a53df71b26c0e292839a656f2a0f8cdfbf2
TimDeBauw1 <47894582+TimDeBauw1@users.noreply.github.com>
2019-04-16 21:58:29 +0200

    Update example.py

--- scripts/example.py
+++ scripts/example.py
@@ -1,16 +1,48 @@
-"""
-This is a dummy script of a 'solution' of the first 3 queries.
-"""
-
-
 def query_01(connection, column_names):
-    return None
+    
+    # Bouw je query
+    query="""
+    SELECT t.name, t.yearID, t.HR 
+    FROM Teams as t
+    ORDER BY t.HR DESC;
+    """.format()
+    
+    # Stap 2 & 3
+    res = run_query(connection, query)         # Query uitvoeren
+    df = res_to_df(res, column_names)          # Query in DataFrame brengen
+    
+    return df
 
-
-def query_02(connection, column_names, datum='1980-01-16'):
-    return None
-
+def query_02(connection, column_names, datum_x='1980-01-16'):
+    
+    # Bouw je query
+    query="""
+    SELECT m.nameFirst, m.nameLast, m.birthYear, m.birthMonth, m.birthDay
+    FROM Master as m
+    WHERE debut > {} 
+    ORDER BY m.nameLast ASC;
+    """.format(datum_x)
+    
+    # Stap 2 & 3
+    res = run_query(connection, query)         # Query uitvoeren
+    df = res_to_df(res, column_names)          # Query in DataFrame brengen
+    
+    return df
 
 def query_03(connection, column_names):
-    return None
+    
+    # Bouw je query
+    query="""
+    SELECT DISTINCT t2.nameFirst, t2.nameLast, t1.teamID
+    FROM (SELECT playerId,teamID FROM Managers m WHERE m.plyrMgr = 'N') as t1, 
+        (SELECT playerID, nameFirst, nameLast FROM Master) as t2 
+    WHERE  t1.playerId = t2.playerID
+    ORDER BY t1.teamID
+    """
+    
+    # Stap 2 & 3
+    res = run_query(connection, query)         # Query uitvoeren
+    df = res_to_df(res, column_names)          # Query in DataFrame brengen
+    
+    return df
 
